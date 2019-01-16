4eeb6ee3f95f491f928ba5bbb5836be870498639
Shion Honda <26x.orc.ed5.1hs@gmail.com>
2019-01-16 21:38:49 +0900

    preprocess for bert

---
+++ src/preprocess_for_bert.py
@@ -0,0 +1,58 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import multiprocessing
+import time
+from tqdm import tqdm
+from rdkit import Chem, DataStructs
+from rdkit.Chem import AllChem
+
+def get_fps(candidates):
+    smiles, fps = [], []
+    for sm in tqdm(candidates[:10000]):
+        mol = Chem.MolFromSmiles(sm)
+        if mol is None:
+            continue
+        smiles.append(sm)
+        fp = AllChem.GetMorganFingerprintAsBitVect(mol, 2, 1024) # ECFP4
+        fps.append(fp)
+    return smiles, fps
+
+def choose_pair(smiles, fps, n,m):
+    N = len(smiles)
+    if m<n-1:
+        lst = smiles[m*(N//n):(m+1)*(N//n)]
+    else:
+        lst = smiles[m*(N//n):]
+    for i,sm in tqdm(enumerate(lst)):
+        tanimotos = np.array(DataStructs.BulkTanimotoSimilarity(fps[i],fps)) # Tanimoto similarity
+        idx = tanimotos.argsort()[::-1][1]
+        with open('../data/chembl_24_bert.csv', 'a') as f:
+            f.write('%s,%s,,,%f\n' %(sm,smiles[idx],tanimotos[idx]))
+
+def main():
+    num_process = 12
+    
+    df = pd.read_csv('../data/chembl_24.csv')
+    candidates = df['canonical_smiles'].values
+    smiles, fps = get_fps(candidates)
+    N = len(smiles)
+    print('N=%d'%N)
+    
+    with open('../data/chembl_24_bert.csv', 'a') as f:
+        f.write('first,second,first_sp,second_sp,tanimoto\n')
+    # Execute multiprocessed choose_pair
+    start = time.time()
+    processes = []
+    for i in range(num_process):
+        process = multiprocessing.Process(target=choose_pair, args=([smiles, fps, num_process,i]))
+        processes.append(process)
+    for process in processes:
+        process.start()
+    for process in processes:
+        process.join()
+    elapsed = time.time()-start
+    print('%f seconds elapsed.' %elapsed)
+
+if __name__=='__main__':
+    main()
