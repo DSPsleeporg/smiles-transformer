f30718640d08b2f9ba90918f5fb38ae635f36d1f
Shion Honda <26x.orc.ed5.1hs@gmail.com>
2019-01-17 18:04:50 +0900

    Add args

--- src/preprocess_for_bert.py
+++ src/preprocess_for_bert.py
@@ -19,38 +19,41 @@ def get_fps(candidates):
         fps.append(fp)
     return smiles, fps
 
-def choose_pair(smiles, fps, n,m):
+def choose_pair(smiles, fps, n, m, out_path):
     N = len(smiles)
     for i in tqdm(range((m*N)//n, ((m+1)*N//n))):
         tanimotos = np.array(DataStructs.BulkTanimotoSimilarity(fps[i],fps)) # Tanimoto similarity
         tanimotos[i] = 0 # Choose second largest (not the self)
         arg_i = np.argmax(tanimotos)
-        with open('../data/chembl_24_bert.csv', 'a') as f:
+        with open(out_path, 'a') as f:
             f.write('%s,%s,,,%f\n' %(smiles[i],smiles[arg_i],tanimotos[arg_i]))
 
 def main():
     parser = argparse.ArgumentParser(description='Make pairs of similar molecules')
     parser.add_argument('--n_process', '-p', type=int, default=1, help='number of processes paralelled')
-    parser.add_argument('--file_path', '-f', type=str, default='../data/chembl24_train.csv', help='specify which file to preprocess')
+    parser.add_argument('--in_path', '-i', type=str, default='../data/chembl24_train.csv', help='input file')
+    parser.add_argument('--out_path', '-o', type=str, default='../data/chembl24_bert_train.csv', help='output file')
     args = parser.parse_args()
     print('%d processes paralelled.' % args.n_process)
-    print('File path: %s' % args.file_path )
+    print('Input file: %s' % args.in_path )
+    print('Output file: %s' % args.out_path )
     print('Start preprocessing')
 
-    n_process = args.n_process   
-    df = pd.read_csv(args.file_path)
+    n_process = args.n_process
+    out_path = args.out_path  
+    df = pd.read_csv(args.in_path)
     candidates = df['canonical_smiles'].values
     smiles, fps = get_fps(candidates)
     N = len(smiles)
     print('N=%d'%N)
     
-    with open('../data/chembl_24_bert.csv', 'a') as f:
+    with open(out_path, 'a') as f:
         f.write('first,second,first_sp,second_sp,tanimoto\n')
     # Execute multiprocessed choose_pair
     start = time.time()
     processes = []
     for i in range(n_process):
-        process = multiprocessing.Process(target=choose_pair, args=([smiles, fps, n_process,i]))
+        process = multiprocessing.Process(target=choose_pair, args=([smiles, fps, n_process,i, out_path]))
         processes.append(process)
     for process in processes:
         process.start()
