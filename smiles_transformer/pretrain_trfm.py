bcf8e4e50e0475946d9968a077dac6f14bc6c15c
Shion Honda <26x.orc.ed5.1hs@gmail.com>
2019-06-28 19:22:01 +0900

    change params of transformer

--- smiles_transformer/pretrain_trfm.py
+++ smiles_transformer/pretrain_trfm.py
@@ -46,7 +46,7 @@ class TrfmSeq2seq(nn.Module):
         self.hidden_size = hidden_size
         self.embed = nn.Embedding(in_size, hidden_size)
         self.pe = PositionalEncoding(hidden_size, dropout)
-        self.trfm = nn.Transformer(d_model=hidden_size, nhead=4, num_encoder_layers=4, num_decoder_layers=4, dim_feedforward=1024)
+        self.trfm = nn.Transformer(d_model=hidden_size, nhead=4, num_encoder_layers=4, num_decoder_layers=4, dim_feedforward=256)
         self.out = nn.Linear(hidden_size, out_size)
 
     def forward(self, src, hidden=None):
@@ -60,32 +60,28 @@ class TrfmSeq2seq(nn.Module):
 
 def parse_arguments():
     parser = argparse.ArgumentParser(description='Hyperparams')
-    parser.add_argument('--n_epoch', '-e', type=int, default=10, help='number of epochs')
+    parser.add_argument('--n_epoch', '-e', type=int, default=5, help='number of epochs')
     parser.add_argument('--vocab', '-v', type=str, default='data/vocab.pkl', help='vocabulary (.pkl)')
     parser.add_argument('--train_data', type=str, default='data/chembl24_bert_train.csv', help='train corpus (.csv)')
     parser.add_argument('--test_data', type=str, default='data/chembl24_bert_test.csv', help='test corpus (.csv)')
     parser.add_argument('--out-dir', '-o', type=str, default='../result', help='output directory')
     parser.add_argument('--name', '-n', type=str, default='ST', help='model name')
     parser.add_argument('--seq_len', type=int, default=220, help='maximum length of the paired seqence')
-    parser.add_argument('--batch_size', '-b', type=int, default=16, help='batch size')
+    parser.add_argument('--batch_size', '-b', type=int, default=32, help='batch size')
     parser.add_argument('--n_worker', '-w', type=int, default=16, help='number of workers')
     parser.add_argument('--hidden', type=int, default=256, help='length of hidden vector')
-    parser.add_argument('--n_layer', '-l', type=int, default=8, help='number of layers')
+    parser.add_argument('--n_layer', '-l', type=int, default=4, help='number of layers')
     parser.add_argument('--n_head', type=int, default=8, help='number of attention heads')
     parser.add_argument('--dropout', '-d', type=float, default=0.1, help='dropout rate')
     parser.add_argument('--lr', type=float, default=1e-4, help='Adam learning rate')
-    parser.add_argument('--lr-decay', type=int, default=50000, help='lr decay step size')
-    parser.add_argument('--log-freq', type=int, default=100, help='log frequency')
     parser.add_argument('--gpu', metavar='N', type=int, nargs='+', help='list of GPU IDs to use')
-    parser.add_argument('--checkpoint', '-c', type=str, default=None, help='Parameter to load')
-    parser.add_argument('-grad_clip', type=float, default=10.0, help='in case of gradient explosion')
     return parser.parse_args()
 
 
 def evaluate(model, val_loader, vocab):
     model.eval()
     total_loss = 0
-    for b, data in tqdm(enumerate(val_loader)):
+    for b, data in enumerate(val_loader):
         data = Variable(data.cuda())
         with torch.no_grad():
             output = model(data, data) # (T,B)
@@ -95,55 +91,43 @@ def evaluate(model, val_loader, vocab):
         total_loss += loss.item()
     return total_loss / len(val_loader)
 
-
-def train(model, optimizer, train_loader, vocab, grad_clip):
-    model.train()
-    total_loss = 0
-    for b,data in tqdm(enumerate(train_loader)):
-        data = torch.t(data.cuda()) # (T,B)
-        optimizer.zero_grad()
-        output = model(data, data) # (T,B,V)
-        loss = F.nll_loss(output[1:].view(-1, len(vocab)),
-                data[1:].contiguous().view(-1), ignore_index=PAD)
-        loss.backward()
-        optimizer.step()
-        total_loss += loss.item()
-
-        if b % 100 == 0 and b != 0:
-            total_loss = total_loss / 100
-            print("[%d][loss:%5.2f][pp:%5.2f]" %
-                  (b, total_loss, math.exp(total_loss)))
-            total_loss = 0
-
-
 def main():
     args = parse_arguments()
     assert torch.cuda.is_available()
 
     vocab = WordVocab.load_vocab(args.vocab)
     print("[!] Instantiating models...")
-    model = TrfmSeq2seq(len(vocab), args.hidden, len(vocab), 4).cuda()
+    model = TrfmSeq2seq(len(vocab), args.hidden, len(vocab), args.n_layer).cuda()
     optimizer = optim.Adam(model.parameters(), lr=args.lr)
     train_dataset = Seq2seqDataset(args.train_data, vocab)
     train_loader = DataLoader(train_dataset, batch_size=args.batch_size, shuffle=True, num_workers=args.n_worker)
     val_dataset = Seq2seqDataset(args.test_data, vocab, is_train=False)
     val_loader = DataLoader(val_dataset, batch_size=args.batch_size, shuffle=False, num_workers=args.n_worker)
     print(model)
+    print('Total parameters:', sum(p.numel() for p in model.parameters()))
 
-    best_val_loss = None
+    best_loss = None
     for e in range(1, args.n_epoch):
-        train(model, optimizer, train_loader, vocab, args.grad_clip)
-        val_loss = evaluate(model, val_loader, vocab)
-        print("[Epoch:%d] val_loss:%5.3f | val_pp:%5.2fS"
-              % (e, val_loss, math.exp(val_loss)))
-
-        # Save the model if the validation loss is the best we've seen so far.
-        if not best_val_loss or val_loss < best_val_loss:
-            print("[!] saving model...")
-            if not os.path.isdir(".save"):
-                os.makedirs(".save")
-            torch.save(model.state_dict(), './.save/trfm_%d.pkl' % (e))
-            best_val_loss = val_loss
+        for b,data in tqdm(enumerate(train_loader)):
+            data = torch.t(data.cuda()) # (T,B)
+            optimizer.zero_grad()
+            output = model(data, data) # (T,B,V)
+            loss = F.nll_loss(output[1:].view(-1, len(vocab)),
+                    data[1:].contiguous().view(-1), ignore_index=PAD)
+            loss.backward()
+            optimizer.step()
+            if b%100==0:
+                print('Train {:3d}: iter {:5d} | loss {:.3f} | ppl {:.3f}'.format(e, b, loss.item(), math.exp(loss.item())))
+            if b%1000==0:
+                loss = evaluate(model, val_loader, vocab)
+                print('Val {:3d}: iter {:5d} | loss {:.3f} | ppl {:.3f}'.format(e, b, loss, math.exp(loss)))
+                # Save the model if the validation loss is the best we've seen so far.
+                if not best_loss or loss < best_loss:
+                    print("[!] saving model...")
+                    if not os.path.isdir(".save"):
+                        os.makedirs(".save")
+                    torch.save(model.state_dict(), './.save/trfm_%d_%d.pkl' % (e,b))
+                    best_loss = loss
 
 
 if __name__ == "__main__":
